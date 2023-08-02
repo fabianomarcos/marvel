e4be624dd801e21bd54edd667bb011ee37cd958d
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-01 22:54:57 -0300

    styles: global styles and theme

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+import { theme } from './theme'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Epilogue', 'Inter', serif;
+  }
+
+  h1 {
+    font-size: 36px;
+    font-weight: 700;
+    letter-spacing: -2.34px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+    DIVIDER: '#EBEFF2',
+    PRIMARY_800: '#00113D',
+    PRIMARY_500: '#293D71',
+    PRIMARY_600: '#213770',
+    PRIMARY_200: '#747D94',
+    ORANGE_500: '#F21A05',
+    ORANGE_400: '#F43724',
+    GRAY: '#F5F6F8',
+    GRAY_100: '#EAECF0',
+    GRAY_150: '#FBFBFB',
+    GRAY_400: '#B7B7B7',
+    GRAY_500: '#777777',
+  },
+}
+
+export type ThemeType = typeof theme
