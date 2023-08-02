6ccff16331cd434b80ddaf050c404655865755e2
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-01 23:07:36 -0300

    feat: base login page

---
+++ src/pages/login/styles.ts
@@ -0,0 +1,19 @@
+import { styled } from 'styled-components'
+import { theme } from '../../styles/theme'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${theme.COLORS.PRIMARY_800};
+`
+export const ContainerImage = styled.div`
+  width: 100vw;
+  padding: 48px 0 0 108px;
+`
+export const BuildImage = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: end;
+  gap: 140px;
+  padding: 100px 0 0 200px;
+`
