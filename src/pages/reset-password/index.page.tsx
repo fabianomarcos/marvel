d2cfd273f3043a3db5c4eaea66aa748a992b7a8a
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-01 23:06:30 -0300

    feat: form for reset password

---
+++ src/pages/reset-password/index.page.tsx
@@ -0,0 +1,6 @@
+import ResetPassword from '../login/components/ResetPassword'
+import LoginPage from '../login/components/LoginPage'
+
+export default function Recovery() {
+  return <LoginPage component={ResetPassword} />
+}
