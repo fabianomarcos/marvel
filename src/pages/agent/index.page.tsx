5e1c2fc79f4e21426bad1d8940c0f9de85de8142
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-06 03:25:02 -0300

    feat: prefer agent

---
+++ src/pages/agent/index.page.tsx
@@ -0,0 +1,6 @@
+import LoginPage from "../login/components/LoginPage";
+import Agent from '../login/components/Agent'
+
+export default function AgentPage() {
+  return <LoginPage component={Agent} />
+}
