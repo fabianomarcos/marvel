96ce5d3139c0d8c6754f1860b889ab82e8d0a8a1
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-06 06:45:16 -0300

    Feat: structure to sigle page

---
+++ src/pages/perfil/[name]/index.page.tsx
@@ -0,0 +1,8 @@
+import { Structure } from '@/pages/dashboard';
+import { Perfil } from '../components';
+
+export default function PerfilPage() {
+  return (
+    <Structure component={Perfil} />
+  )
+}
