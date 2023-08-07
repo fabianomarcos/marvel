611bf86172d887940a9a671c3a8ef07efa84629b
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-07 19:41:12 -0300

    feat: get agents by name with limit per page

--- src/hooks/store.tsx
+++ src/hooks/store.tsx
@@ -1,41 +1,54 @@
-import React, { createContext, useCallback, useState, useContext, useEffect } from 'react'
-import { apiMarvel } from '@/lib/axios'
-import { configMarvelApi } from '@/utils/configMarvelApi'
-import { IInfoCharacters } from '@/interfaces/types'
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext
+} from "react";
+import { apiMarvel } from "@/lib/axios";
 
-interface StoreContextData {
-  characters: IInfoCharacters[]
+import { configMarvelApi } from "@/utils/configMarvelApi";
+import { IInfoCharacters } from "@/interfaces/types";
+
+interface IGetAgents {
+  limit: number;
+  name?: string;
 }
 
-const StoreContext = createContext<StoreContextData>({} as StoreContextData)
+interface StoreContextData {
+  characters: IInfoCharacters[];
+  totalCharacters: number;
+  getAgents: ({ limit, name = "" }: IGetAgents) => Promise<void>;
+}
+
+const StoreContext = createContext<StoreContextData>({} as StoreContextData);
 
 function StoreProvider({ children }: any) {
-  const [characters, setCharacters] = useState<IInfoCharacters[]>([])
-  const { timestamp, apiKey, hash } = configMarvelApi
-  const request = apiMarvel.get(
-    `/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}`
-  );
+  const [characters, setCharacters] = useState<IInfoCharacters[]>([]);
+  const [totalCharacters, setTotalCharacters] = useState(0);
+  const { timestamp, apiKey, hash } = configMarvelApi;
 
-  useEffect(() => {
-    const getAgents = async () => {
-      const { data: { data } } = await request
-      setCharacters(data.results);
-    }
-    getAgents()
-  }, [])
+  const getAgents = useCallback(async ({ limit, name }: IGetAgents) => {
+    const nameStartsWith = name ? `&nameStartsWith=${name}` : "";
+    const request = apiMarvel.get(
+      `/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}&limit=${limit}${nameStartsWith}`
+    );
+    const {
+      data: { data },
+    } = await request;
+    setCharacters(data.results);
+    setTotalCharacters(data.total);
+  }, []);
 
   return (
-    <StoreContext.Provider
-      value={{ characters }}
-    >
+    <StoreContext.Provider value={{ characters, totalCharacters, getAgents }}>
       {children}
     </StoreContext.Provider>
-  )
+  );
 }
 
 function useStore(): StoreContextData {
-  const context = useContext(StoreContext)
-  return context
+  const context = useContext(StoreContext);
+  return context;
 }
 
-export { StoreProvider, useStore }
+export { StoreProvider, useStore };
