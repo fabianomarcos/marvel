56cc00907152145dfb901f5d092a7850b7548911
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-07 14:19:57 -0300

    feat: search characters

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
-import { Container } from './styles'
+import { useSearch } from "@/hooks/useDebounce";
+import { Container, Search } from "./styles";
+import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
 
-interface IProps {
-  component?: any
-}
+export default function Header() {
+  const [inputValue, setInputValue] = useState("");
+  const value = useSearch({ delay: 3000, value: inputValue });
+  console.log('value: ', value);
 
-export default function Header({ component: Component }: IProps) {
+  const getSearchParams = (param: string) => setInputValue(param);
+
   return (
     <Container>
-
+      <Search>
+        <FiSearch size={20} />
+        <input
+          placeholder="Busque um agente"
+          onChange={(event) => getSearchParams(event.target.value)}
+        />
+      </Search>
     </Container>
-  )
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,3 +5,25 @@ export const Container = styled.div`
   height: 60px;
   border-bottom: 1px solid ${({ theme }) => theme.COLORS.ORANGE_500};
 `
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-left: 37px;
+  gap: 16px;
+
+  input {
+    width: 230px;
+    border-radius: 15px;
+    border: 1px solid ${({ theme }) => theme.COLORS.GRAY_100};
+    padding: 10px;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.PRIMARY_200};
+      text-align: left;
+      font-size: 12px;
+      font-weight: 500;
+      letter-spacing: -0.36px;
+    }
+  }
+`
