ae1f71c6089e05f44b45eb02d09747c39b06c3bc
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-07 22:44:33 -0300

    STYLES: fix divider color

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,7 @@ import { styled } from 'styled-components'
 export const Container = styled.div`
   width: 100%;
   height: 60px;
-  border-bottom: 1px solid ${({ theme }) => theme.COLORS.ORANGE_500};
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.DIVIDER};
 `
 export const Search = styled.div`
   display: flex;
