e3c5d2c2353481923a3030c1aded11d34bd86acf
Fabiano Marcos <fabianofmarcos@gmail.com>
2023-08-08 11:59:06 -0300

    styles: fix login page

--- src/pages/login/components/LoginPage/styles.ts
+++ src/pages/login/components/LoginPage/styles.ts
@@ -1,18 +1,26 @@
-import { styled } from 'styled-components'
+import { styled } from "styled-components";
 
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
   background: ${({ theme }) => theme.COLORS.PRIMARY_800};
-`
+  min-width: 1800px;
+  min-height: 1160px;
+
+  @media (max-width: 1500px) {
+    min-width: 700px;
+    min-height: 1000px;
+    gap: 140px;
+  }
+`;
+
 export const ContainerImage = styled.div`
-  width: 100vw;
+  width: 100%;
   padding: 48px 0 0 108px;
-`
+`;
+
 export const BuildImage = styled.div`
   display: flex;
-  justify-content: start;
+  justify-content: space-around;
   align-items: end;
   gap: 140px;
-  padding: 100px 0 0 200px;
-`
+  padding: 100px 200px;
+`;
