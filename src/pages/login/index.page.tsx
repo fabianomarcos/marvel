c8ade59e20d34fddb38899c65261a1135f1181cb
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-01-27 19:39:23 -0300

    testes

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -2,6 +2,5 @@ import FormLogin from "./components/FormLogin";
 import LoginPage from "./components/LoginPage";
 
 export default function Login() {
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
   return <LoginPage component={FormLogin} />;
 }
